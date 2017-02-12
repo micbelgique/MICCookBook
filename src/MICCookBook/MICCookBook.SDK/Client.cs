fefd17849b0eeb84c638d7388b3b23beedfb029c
Renaud Dumont <renaud.dumont@live.fr>
2017-02-12 17:13:09 +0100

    Added CookBookClient in the SDK project.

---
+++ src/MICCookBook/MICCookBook.SDK/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MICCookBook.SDK.Model;
+using Newtonsoft.Json;
+
+namespace MICCookBook.SDK
+{
+    /// <summary>
+    /// TODO : keep one instance of HttpClient accross multiple calls
+    /// </summary>
+    public class Client
+    {
+        public HttpClient HttpClient { get; set; }
+
+        public Client()
+        {
+            HttpClient = new HttpClient();
+            HttpClient.BaseAddress = new Uri("http://localhost:2011/api");
+        }
+
+        public async Task<List<Recipe>> GetRecipes()
+        {
+            var content = await HttpClient.GetStringAsync("recipes");
+            var recipes = JsonConvert.DeserializeObject<List<Recipe>>(content);
+            return recipes;
+
+        }
+    }
+}
